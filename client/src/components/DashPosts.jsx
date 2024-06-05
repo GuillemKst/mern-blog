cb2df6d220776ecd4786c423f0302589e20befee
Guillem Castells <guillemcastellsf@gmail.com>
2024-06-05 16:15:19 +0200

    Add show more functionality on post Dashboard Adm

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom'
 export default function DashPosts() {
 const {currentUser} = useSelector(state => state.user)
 const [userPosts, setUserPosts] = useState([])
-console.log(userPosts)
+const [showMore, setShowMore] = useState(true)
     useEffect(() => {
         const fetchPosts = async () => {
             try {
@@ -16,7 +16,9 @@ console.log(userPosts)
                 const data = await res.json()
                 if(res.ok){
                     setUserPosts(data.posts)
-                }
+                    if(data.posts.length < 9){
+                        setShowMore(false);
+                }}
             } catch (error) {
                 console.log(error.message)
             }
@@ -25,6 +27,23 @@ console.log(userPosts)
             fetchPosts();
         }
     }, [currentUser._id])
+
+    const handleShowMore = async () => {
+        const startIndex = userPosts.length;
+        try {
+            const res= await fetch(`api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+            const data = await res.json();
+            if(res.ok){
+                setUserPosts(prev => [...prev, ...data.posts]);
+                if(data.posts.length < 9){
+                    setShowMore(false);
+                }
+            }
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-300 '>
         {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -65,6 +84,13 @@ console.log(userPosts)
                 ))}
 
             </Table>
+            {
+                showMore && (
+                    <div className='flex justify-center mt-5'>
+                        <button onClick={handleShowMore} className=' dark:text-white px-4 py-2 rounded-md mt-2 hover:underline'>Show more</button>
+                    </div>
+                )
+            }
             </>
         ):(
             <p>You have no posts yet</p>)}
