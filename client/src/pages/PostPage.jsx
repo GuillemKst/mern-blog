be7241fa5f62f3559b2e2843cbd3cd06f618d7f7
Guillem Castells <guillemcastellsf@gmail.com>
2024-06-11 11:29:32 +0200

    Add CTA to the Post Page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@
 import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import CallToAction from '../components/CallToAction';
 
 export default function PostPage() {
     const { postSlug } = useParams();
@@ -45,7 +46,7 @@ export default function PostPage() {
     <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
       <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>{post && post.title}</h1>
       <Link to={`/search?category=${post && post.category}`} className='self-center mt-5'>
-      <Button color='gray' pill size='xs' >{post && post.category} </Button>
+      <Button color='gray' pill size='xs' className='post-category'>{post && post.category} </Button>
       </Link>
       <img src={post && post.image} alt={post && post.title} className='mt-10 max-h-[600px] p-3 w-full object-cover  '/>
       <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
@@ -53,7 +54,10 @@ export default function PostPage() {
         <span className='italic'> {post && (post.content.length /1000).toFixed(0)} mins read</span>
       </div>
       <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post && post.content}}>
-    
+          
+      </div>
+      <div className='max-w-4xl mx-auto w-full'>
+        <CallToAction/>
       </div>
     </main>
   )
