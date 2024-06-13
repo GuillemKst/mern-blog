d97a1fd20cc6438bc53d3f01778a95452f5cb8c6
Guillem Castells <guillemcastellsf@gmail.com>
2024-06-13 17:48:19 +0200

    Complete home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,36 +1,52 @@
-import React from 'react'
-import CardStack from '../components/CardStack'
-import CardStackControls from '../components/CardStackControls';
-
-const items = [
-    {
-      id: 1,
-      name: "John Doe",
-      designation: "Software Engineer",
-      content: <p>This is some content for John Doe's card.</p>,
-    },
-    {
-      id: 2,
-      name: "Jane Smith",
-      designation: "Product Manager",
-      content: <p>This is some content for Jane Smith's card.</p>,
-    },
-    {
-      id: 3,
-      name: "Sam Johnson",
-      designation: "Designer",
-      content: <p>This is some content for Sam Johnson's card.</p>,
-    },
-    // Add more items as needed
-  ];
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import CallToAction from '../components/CallToAction';
+import Post from '../../../api/models/post.model';
+import PostCard from '../components/PostCard';
 
 export default function Home() {
-    return (
-        <div className="p-10">
-          <CardStack items={items} />
-          <CardStackControls/>
-    
+  const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+        const res = await fetch('/api/post/getposts');
+        const data = await res.json();
+        setPosts(data.posts);
+
+    };
+    fetchPosts();
+  }, []);
+    return (
+        <div className="">
+          <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+            <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to ###</h1>
+            <p className='text-gray-500 text-xs sm:text-sm'> Our cutting-edge SaaS platform empowers your temas to treamline workflows, booost productivity, and drive sustainable growth.</p>
+            <Link to='/search' className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'>View all posts</Link>
+          </div>
+          <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+            <CallToAction/>
+          </div>
+          <div className='max-w-full mx-auto p-3 flex flex-col gap-8 py-7'>
+            {
+              posts && posts.length > 0 && (
+                <div className='flex flex-col gap-6 p-7'>
+                  <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+                  <div className='flex flex-wrap gap-4 justify-center'>
+                    {
+                      posts.map((posts) => (
+                        
+                          <PostCard post={posts} key={posts._id}/>
+                        
+                      ))
+                    }
+                  </div>
+                  <Link to={'/search'} className='text-lg text-teal-500 hover:underline text-center'>
+                  View al posts
+                  </Link>
+                </div>
+              )
+            }
+          </div>
         </div>
       );
 }
