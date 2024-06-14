d94ab7a0f0ac392cfc8be5348d25e9c2a3baf54e
Guillem Castells <guillemcastellsf@gmail.com>
2024-06-14 17:27:11 +0200

    Add about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div className=''>
+          <h1 className='text-3xl font-semibold text-center my-7'>About</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p className=''>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum at sapien sit amet posuere. Cras viverra egestas diam, ut luctus ipsum pellentesque in. Morbi luctus nec nibh non facilisis. Aliquam sed ante non felis posuere viverra vel vel dolor. 
+            </p>
+            <p className=''>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum at sapien sit amet posuere. Cras viverra egestas diam, ut luctus ipsum pellentesque in. Morbi luctus nec nibh non facilisis. Aliquam sed ante non felis posuere viverra vel vel dolor. 
+            </p>
+            <p className=''>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum at sapien sit amet posuere. Cras viverra egestas diam, ut luctus ipsum pellentesque in. Morbi luctus nec nibh non facilisis. Aliquam sed ante non felis posuere viverra vel vel dolor. 
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import CallToAction from '../components/CallToAction'
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen max-w-2xl  mx-auto flex mt-8 flex-col gap-6 items-center'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-md text-gray-500 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum at sapien sit amet posuere.</p>
+      <CallToAction />
+    </div>
   )
 }
